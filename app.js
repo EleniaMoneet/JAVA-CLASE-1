2658e90382b4d4b055eb553ca40692ff69af6652
Ayelen Gabrielli <moneet.moneet@gmail.com>
2021-07-28 20:34:21 -0300

    COMMIT UNO

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let nombre = prompt ("Ingresa Nombre ")
+let apellido = prompt ("Ingresa Apelido")
+let signo = prompt ("De que signo es?")
+
+alert(nombre + " " + apellido + " Usted Está Loco")
+
+console.log (nombre + " " + apellido + " " + signo) 
+
